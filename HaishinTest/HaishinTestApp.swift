4686f88c6fa59b6ee992a1c92caa1090b8cc4a03
shunchobi <shunbeat1008@gmail.com>
2022-07-11 17:25:20 +0900

    Initial Commit

---
+++ HaishinTest/HaishinTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaishinTestApp.swift
+//  HaishinTest
+//
+//  Created by Shun on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct HaishinTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
