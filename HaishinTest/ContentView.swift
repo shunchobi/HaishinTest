e2ce0e8fe89162c6829eba304b19f25ee1929977
shunchobi <shunbeat1008@gmail.com>
2022-07-11 19:53:47 +0900

    real time messaging video app with HaishinKit

--- HaishinTest/ContentView.swift
+++ HaishinTest/ContentView.swift
@@ -5,17 +5,44 @@
 //  Created by Shun on 2022/07/11.
 //
 
+import AVFoundation
+import HaishinKit
 import SwiftUI
+import VideoToolbox
 
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+
+    private var lfView: MTHKSwiftUiView!
+    private var menuView: MenuView!
+
+    init() {
+        viewModel.config()
+        lfView = MTHKSwiftUiView(rtmpStream: $viewModel.rtmpStream)
+        menuView = MenuView(viewModel: viewModel)
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            lfView
+                .ignoresSafeArea()
+                .onTapGesture { location in
+                    self.viewModel.tapScreen(touchPoint: location)
+                }
+
+            menuView
+        }
+        .onAppear {
+            self.viewModel.registerForPublishEvent()
+        }
+        .onDisappear {
+            self.viewModel.unregisterForPublishEvent()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
